ba2647d2e2fdd9d5b1cf3c4401b3fe70ed44cd53
Jorge <jf69766@gmail.comgit config --global user.email :jf69766@gmail.com>
2026-04-27 01:15:08 -0600

    latest update

--- script.js
+++ script.js
@@ -36,13 +36,13 @@ function Completar(){
 
     let paciente = (nombre == '' && apellido == '') ? 'Paciente':`Paciente ${nombre} ${apellido}`;
     let edadR = (dob == null) ? 'edad desconocida':`${edad} años`;
-    let cambiosText = (cambios == 'ninguno' || cambios == 'no' || cambios == '' || cambios == 'no cambios' || cambios == 'no refiere cambios') ? 'no refiere cambios':`refiere ${cambios}`;
+    let cambiosText = (cambios == 'ninguno' || cambios == 'no' || cambios == '' || cambios == 'no cambios' || cambios == 'no refiere cambios') ? 'no refiere cambios':`refiere haber implementado cambios desde la consulta anterior consistentes en ${cambios}`;
     let barrera = (barreas == '' || barreas == 'no' || barreas == 'ninguno' || barreas == 'no barreras' || barreas == 'no identifica barreras') ? 'no identifica barreras':`identifica como principales barreras ${barreas}`;
     let avancesR = (avances == '' || avances == 'no' || avances == 'ninguno' || avances == 'no avances' || avances == 'no percibe avances') ? 'No percibe avances':`Percibe como avances ${avances}`;
-    let ajustesR = (ajustes == '' || ajustes == 'no' || ajustes == 'ninguno' || ajustes == 'no ajustes' || ajustes == 'no se realizaron avances') ? 'no se ningun ajuste ajustes':`se realizaron ajustes al plan nutricional ${ajustes}`;
+    let ajustesR = (ajustes == '' || ajustes == 'no' || ajustes == 'ninguno' || ajustes == 'no ajustes' || ajustes == 'no se realizaron avances') ? 'no se ningun ajuste ajustes':`se realizaron ajustes al plannutricional ${ajustes}`;
     let estadoR = (estado == '' || estado == 'no' || estado == 'ninguno' || estado == 'nada') ? 'no percibe cambios desde la ultima cita':`refiere ${estado}`;
     let CambiosER = (cambiosE == '' || cambiosE == 'no' || cambiosE == 'ninguno' || cambiosE == 'sin cambios') ? 'no registra cambios de energia':`${cambiosE}`;
-    let sintomasR = (sintomas == '' || sintomas == 'no' || sintomas == 'ninguno' || sintomas == 'nada' || sintomas == 'No sintomas' || sintomas == 'no percibe sintomas') ? 'No percibe sintomas':`${sintomas}`;
+    let sintomasR = (sintomas == '' || sintomas == 'no' || sintomas == 'ninguno' || sintomas == 'nada' || sintomas == 'No sintomas' || sintomas == 'no percibe sintomas digestivos') ? 'No percibe sintomas digestivos':`${sintomas}`;
     let dificultadesR = (dificultades == '' || dificultades == 'no' || dificultades == 'ninguna' || dificultades == 'sin dificultades') ? 'sin dificultades en la medicion':`${dificultades}`;
     let momentoR = (momento == '' || momento == 'no' || momento == 'ninguno' || momento == 'sin momento especifico') ? 'no tiene un momento especifico que le cause dificultades':`identifica ${momento} como su momento mas dificil`;
     let situacionesR = (situaciones == '' || situaciones == 'no' || situaciones == 'ninguna' || situaciones == 'sin situaciones especificas') ? 'no idenetifica situaciones que le causen dificultades':`identifica ${situaciones} como situaciones que le causan dificultad`;
@@ -51,8 +51,8 @@ function Completar(){
     let difiRultadesEjeR = (dificultadesEje == '' || dificultadesEje == 'no' || dificultadesEje == 'ninguna' || dificultadesEje == 'sin dificultades') ? 'no percibe dificultades para cumplir el ejercicio':`${dificultadesEje}`;
     let estrategiasR = (estrategias == '' || estrategias == 'no' || estrategias == 'ninguna' || alimentos == 'sin estrategias') ? 'no utilizó ninguna estrategia':`${estrategias}`;
 
-    let text = `${paciente}, ${sexo} de ${edadR} en seguimiento nutricional para ${seguimiento}, quien ${cambiosText}; ${barrera}. ${avancesR}, 
-    por lo que ${ajustesR}. Clínicamente ${estadoR}, ${sintomasR}, ${CambiosER}. En evaluación antropométrica reporta ${registro}, ${dificultadesR}, ${cambiosR}. 
+    let text = `Paciente ${sexo} de ${edadR} en seguimiento nutricional para ${seguimiento}, quien ${cambiosText}; ${barrera}. ${avancesR}, 
+    por lo que ${ajustesR}. Clínicamente ${estadoR}, ${sintomasR},  ${sintomasR}, ${CambiosER}. En evaluación antropométrica reporta ${registro}, ${dificultadesR}. 
     Dietéticamente ha ${cambiosAliR}, ${alimentosR}, ${cumplimientos}, con adherencia estimada de ${adherencia}; ${momentoR}, ${situacionesR}. Recuento de 24 horas ${recuento}. 
     ${rutina}, ${difiRultadesEjeR}. Duerme ${sueño}, consume ${agua} de agua y reporta nivel de estrés ${estres},
     ${estrategiasR} ${emociones}. ${refuerzo}`;
